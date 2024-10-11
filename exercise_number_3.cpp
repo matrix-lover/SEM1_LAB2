1ac2bdc6b01f91836ef4ff680557444d6937668f
Shamil Krut <gmific@yandex.ru>
2024-10-11 13:34:52 -0700

    Update exercise_number_3.cpp

--- exercise_number_3.cpp
+++ exercise_number_3.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main() {
     
 //part num 1:
+    cout << "Таблица умножения на девять:" << endl;
     int a = 7;
     int i;
     int N;
@@ -16,6 +17,8 @@ int main() {
         cout << i << " * " << a << " = " << i * a << endl;
 
 //part num 2:
+    cout << "Введите целое число: ";
+    
     cin >> N;
     
     for (i = 1; i < 10; ++i)
