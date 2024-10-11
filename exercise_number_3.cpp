b26a28e75acf00051f0a62ee78d171fbf2a89553
Shamil Krut <gmific@yandex.ru>
2024-10-11 13:17:45 -0700

    Update exercise_number_3.cpp

--- exercise_number_3.cpp
+++ exercise_number_3.cpp
@@ -6,10 +6,19 @@
 using namespace std;
 
 int main() {
+    
+//part num 1:
     int a = 7;
     int i;
+    int N;
     
     for (i = 1; i < 10; ++i)
         cout << i << " * " << a << " = " << i * a << endl;
+
+//part num 2:
+    cin >> N;
+    
+    for (i = 1; i < 10; ++i)
+        cout << i << " * " << N << " = " << i * N << endl;
     return 0;
 }
