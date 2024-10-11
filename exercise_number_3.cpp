d4818a3dc8982be957775444e3e0ded2c0a02858
Shamil Krut <gmific@yandex.ru>
2024-10-11 13:13:00 -0700

    Update exercise_number_3.cpp

--- exercise_number_3.cpp
+++ exercise_number_3.cpp
@@ -1,3 +1,15 @@
 // number 3
 //
-//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 7;
+    int i;
+    
+    for (i = 1; i < 10; ++i)
+        cout << i << " * " << a << " = " << i * a << endl;
+    return 0;
+}
